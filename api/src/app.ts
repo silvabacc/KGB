54c0d94b2bf07dde1d2d4dbd5848486812c16381
k-h-f <>
2022-05-07 23:38:39 +0100

    add start run and update dockerfile

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import routes from './routes';
-import { getConfig } from '../../src/config/getConfig';
+import { getConfig } from './getConfig';
 
 const { PORT } = getConfig();
 

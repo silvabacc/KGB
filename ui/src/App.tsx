0185b0c88dc1e19eade2fb664f11228b017faef4
k-h-f <47926269+k-h-f@users.noreply.github.com>
2022-05-22 04:46:09 +0100

    add healthcheck

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css'
+import './App.css';
 import React from 'react';
 import Home from './home/Home';
+import HealthCheck from './healthcheck/healthcheck';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <Home />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/healthcheck" element={<HealthCheck />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ ui/src/healthcheck/healthcheck.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HealthCheck: React.FC = () => <div>UP</div>;
+
+export default HealthCheck;

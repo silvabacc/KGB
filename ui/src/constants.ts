c2aa18eba554cef1ba636e84a81633875be239ef
k-h-f <47926269+k-h-f@users.noreply.github.com>
2022-05-22 05:30:39 +0100

    update link

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -1,2 +1,2 @@
-// export const KGB_API_URL = 'https://kgb-api.herokuapp.com';
+// export const KGB_API_URL = 'https://kgb-web.nasdiscordbots.repl.co';
 export const KGB_API_URL = 'http://localhost:4000'; //Uncomment this to run locally

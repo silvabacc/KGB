c290d71004799e28d0a9091e708bb2c1869f4a6a
k-h-f <47926269+k-h-f@users.noreply.github.com>
2022-06-08 00:39:30 +0100

    handle 429 http errors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { exec } from 'child_process';
 dotenv.config();
 
 // Require the necessary discord.js classes
@@ -17,4 +18,20 @@ const eventHandler = new EventHandler(client);
 eventHandler.initEvents();
 
 // Login to Discord with your client's token
-client.login(DISCORD_TOKEN);
+client.login(DISCORD_TOKEN).catch((error) => {
+  //Kill the current container if there was a problem connecting (a reset)
+  exec('kill 1', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  //Attempt to reconnect after the kill. If this doesn't work, then something went horribly wrong
+  client.login(DISCORD_TOKEN).catch((error) => console.log(error));
+});
+client.on('debug', (e) => console.log(e));

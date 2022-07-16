29675a9bc517c799896e98f21140645b6b149411
k-h-f <47926269+k-h-f@users.noreply.github.com>
2022-07-16 03:01:10 +0100

    ui improvements

--- ui/src/home/components/MonthlyStats.tsx
+++ ui/src/home/components/MonthlyStats.tsx
@@ -1,8 +1,9 @@
 import './MonthlyStats.css';
-import '../Home.css'
+import '../Home.css';
 import React from 'react';
 import { Month } from '../types';
 import { TimeSeriesResponse } from '../../responseTypes';
+import { defaultColors } from './chartDefaultColors';
 
 interface MonthlyStatsProps {
   monthSelected: Month;
@@ -18,23 +19,23 @@ const MonthlyStats: React.FC<MonthlyStatsProps> = ({ monthSelected, data }) => {
     <>
       <div className="Card MonthlyStatesWrapper">
         <div className="Header">Total stats for {monthSelected} </div>
-        <ol className="List">
+        <ul className="List">
           {data.length ? (
             orderedUsersData
               .filter((data) => data.monthly > 0.01)
-              .map((user) => {
+              .map((user, index) => {
                 return (
                   <li className="Individuals">
-                    {user.name} with {' '}
-                      <span className="Hours">{user.monthly.toFixed(1)}</span>{' '}
-                      hours{' '}
+                    <span style={{color: defaultColors[index], fontWeight: 700}}>{user.name}</span> with{' '}
+                    <span className="Hours">{user.monthly.toFixed(1)}</span>{' '}
+                    hours{' '}
                   </li>
                 );
               })
           ) : (
             <div className="Individuals">There are no stats for this month</div>
           )}
-        </ol>
+        </ul>
       </div>
     </>
   );
---
+++ ui/src/home/components/chartDefaultColors.ts
@@ -0,0 +1,1 @@
+export const defaultColors = ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
